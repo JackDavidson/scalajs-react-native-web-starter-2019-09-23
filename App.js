80e1ada669ad72fdb579c783dca33100e2ed16b2
Jack Davidson <jack.davidson38@gmail.com>
2019-09-25 22:58:50 -0700

    added react native svg and moved icon.png and splash.png back where they were originally

--- App.js
+++ App.js
@@ -24,8 +24,33 @@ const styles = StyleSheet.create({
 
 import React from "react";
 import HybridApp from "./src/js/App";
-import {Platform, View} from "react-native";
+import {Platform, View, StyleSheet} from "react-native";
 import * as Font from "expo-font"
+
+/*import Svg, {
+  Circle,
+  Ellipse,
+  G,
+  Text,
+  TSpan,
+  TextPath,
+  Path,
+  Polygon,
+  Polyline,
+  Line,
+  Rect,
+  Use,
+  Image,
+  Symbol,
+  Defs,
+  LinearGradient,
+  RadialGradient,
+  Stop,
+  ClipPath,
+  Pattern,
+  Mask,
+} from 'react-native-svg';
+*/
 
 export default class NativeApp extends React.Component {
     constructor() {
@@ -61,6 +86,36 @@ export default class NativeApp extends React.Component {
 
     render() {
         const { fontsAreLoaded } = this.state
+	    /*
+	    return (
+      <View
+        style={[
+          StyleSheet.absoluteFill,
+          { alignItems: 'center', justifyContent: 'center' },
+        ]}
+      >
+        <Svg height="50%" width="50%" viewBox="0 0 100 100">
+          <Circle
+            cx="50"
+            cy="50"
+            r="45"
+            stroke="blue"
+            strokeWidth="2.5"
+            fill="green"
+          />
+          <Rect
+            x="15"
+            y="15"
+            width="70"
+            height="70"
+            stroke="red"
+            strokeWidth="2"
+            fill="yellow"
+          />
+        </Svg>
+      </View>
+    );
+    */
         return !fontsAreLoaded ? <View/> : <HybridApp lockOrientation={this.lockOrientation}/>
     }
 }
